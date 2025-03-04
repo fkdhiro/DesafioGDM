85bea071f7570d241526b2bc615af816c9a2f47e
Hiroshi <w.hiroshi.fkd@gmail.com>
2025-03-04 20:51:15 -0300

    vercel

--- back-end-developer-test/application/src/infrastructure/web/cors/cors.service.ts
+++ back-end-developer-test/application/src/infrastructure/web/cors/cors.service.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: [ "http://localhost:3000", "https://seu-dominio.com", 'http://127.0.0.1:5500', "https://abundant-simplicity-production.up.railway.app"],
+  origin: [ "http://localhost:3000", "https://seu-dominio.com", 'http://127.0.0.1:5500', "https://desafio-gdm.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true,
